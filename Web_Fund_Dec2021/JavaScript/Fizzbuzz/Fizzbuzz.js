6d14643937fceefb21d4ac3f38c7967856aada5a
BrandonGriffith <BrandonGriffith92@gmail.com>
2021-12-17 19:48:57 -0500

    Updated

--- Web_Fund_Dec2021/JavaScript/Fizzbuzz/Fizzbuzz.js
+++ Web_Fund_Dec2021/JavaScript/Fizzbuzz/Fizzbuzz.js
@@ -1,11 +1,14 @@
-for(var i=1; i<101; i++) {
-    if(i % 15 == 0) {
+for(var i=1; i<=100; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
-    } else if(i % 5 == 0) {
-        console.log("Buzz");
-    } else if(i % 3 == 0) {
+    } 
+    else if(i % 3 == 0) {
         console.log("Fizz");
-    } else {
+    } 
+    else if(i % 5 == 0) {
+        console.log("Buzz");
+    } 
+    else {
         console.log(i);
     }
 }
