152fc2cc138f8eba1a24bcf55368d11238bc808b
BrandonGriffith <BrandonGriffith92@gmail.com>
2021-12-17 19:31:40 -0500

    Updated

--- ALGOS/w1d5.js
+++ ALGOS/w1d5.js
@@ -57,3 +57,30 @@ function reverse3(x){
 }
 reverse3(y);
 reverse3(y2);
+//other JS
+
+console.log(8 % 3);
+
+var x = 0;
+for(var i=1; i<5; i++) {
+    x += i;
+}
+console.log(x);
+
+function Something(arr){
+    var result = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > 10){
+            console.log("We got a big one!");
+        } 
+        else {
+            result+=arr[i]
+        }
+    }
+    if(result < 10){
+        console.log("You're killin' me, Smalls");
+    } else {
+        return "You're gonna need a bigger funcion!";
+    }
+}
+Something([1,8,21,-11,12,2,9,15,7]);
--- pre_JS/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-console.log('hello world ' + "HelloWorld " + 'Hello World');
-for (var i = 0; i < 50;) {
-    i++
-    i++
-    console.log('hello friend ' + "good bye friend " + i)
-}
