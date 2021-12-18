e0df7426e856abc052f7bd159f3b4f0cf3e1cddf
BrandonGriffith <BrandonGriffith92@gmail.com>
2021-12-17 23:04:35 -0500

    Updated

---
+++ Web_Fund_Dec2021/JavaScript/LoopChallenges/LoopChallenges.js
@@ -0,0 +1,29 @@
+for(i=1;i<=20;i++){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+}
+console.log("end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+for(i=100;i>0;i--){
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}
+console.log("end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+var arry1 = [4, 2.5, 1, -0.5, -2, -3.5];
+for(i=0;i<arry1.length;i++){
+    console.log(arry1[i]);
+}
+console.log("end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+var sum = 0;
+for(i=1;i<=100;i++){
+    sum += i;
+}
+console.log(sum);
+console.log("end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+var product = 1;
+for(i=1;i<=12;i++){
+    product *= i;
+}
+console.log(product);
+console.log("end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
